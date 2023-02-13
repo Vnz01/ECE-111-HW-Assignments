23dcbe6a2a450712e58a82e685ed65f803fe5902
Vnz01 <111817997+Vnz01@users.noreply.github.com>
2023-02-12 23:47:25 -0800

    Update lfsr.sv

--- ECE-111-HW-Assignments/Tech Assignment 5/lfsr.sv
+++ ECE-111-HW-Assignments/Tech Assignment 5/lfsr.sv
@@ -7,20 +7,30 @@ module lfsr
   output logic lfsr_done,
   output logic[N-1:0] lfsr_data
 );
+	
+logic XOR_out;
 
+//always_comb 
 // N = 2, Poly: X^2 + X^1 + 1
-	// XOR Out = lsfr_data[1] ^ lsft_data[0]
+	// XOR_out = lsfr_data[1] ^ lsft_data[0]
+	// lsft_data[0] <= XOR_out
 // N = 3, Poly: X^3 + X^2 + 1
-	// XOR Out = lsfr_data[2] ^ lsft_data[1]
+	// XOR_out = lsfr_data[2] ^ lsft_data[1]
+	// lsft_data[0] <= XOR_out
 // N = 4, Poly: X^4 + X^3 + 1
-	// XOR Out = lsfr_data[3] ^ lsft_data[2]
+	// XOR_out = lsfr_data[3] ^ lsft_data[2]
+	// lsft_data[0] <= XOR_out
 // N = 5, Poly: X^5 + X^3 + 1	
-	// XOR Out = lsfr_data[4] ^ lsft_data[2]
+	// XOR_out = lsfr_data[4] ^ lsft_data[2]
+	// lsft_data[0] <= XOR_out
 // N = 6, Poly: X^6 + X^5 + 1	
-	// XOR Out = lsfr_data[5] ^ lsft_data[4]
+	// XOR_out = lsfr_data[5] ^ lsft_data[4]
+	// lsft_data[0] <= XOR_out
 // N = 7, Poly: X^7 + X^6 + 1	
-	// XOR Out = lsfr_data[6] ^ lsft_data[5]
+	// XOR_out = lsfr_data[6] ^ lsft_data[5]
+	// lsft_data[0] <= XOR_out
 // N = 8, Poly: X^8 + X^6 + X^5 + X^4 + 1
-	// XOR Out = lsfr_data[7] ^ lsft_data[5] ^ lsfr_data[4] ^ lsft_data[3]
+	// XOR_out = lsfr_data[7] ^ lsft_data[5] ^ lsfr_data[4] ^ lsft_data[3]
+	// lsft_data[0] <= XOR_out
 	
 endmodule: lfsr
