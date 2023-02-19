8dfc92791e9ab0ce8b5962c4ea43157f832418e6
Vnz01 <111817997+Vnz01@users.noreply.github.com>
2023-02-18 23:18:05 -0800

    Update carry_lookahead_adder.sv

--- ECE-111-HW-Assignments/Tech Assignment 6/carry_lookahead_adder/carry_lookahead_adder.sv
+++ ECE-111-HW-Assignments/Tech Assignment 6/carry_lookahead_adder/carry_lookahead_adder.sv
@@ -4,8 +4,23 @@ module carry_lookahead_adder#(parameter N=4)(
   input logic CIN,
   output logic[N:0] result
 );
-
- // Add code for carry lookahead adder 
   
+  logic [N-1:0] gen;
+  logic [N-1:0] prop;
+  logic [N-1:0] sum;
+  logic [N:0] car;
+  assign car[0] = CIN;
+  
+  genvar i;
+  generate
+    for (i=0; i<N; i++) begin
+      assign prop[i] = A[i] ^ B[i];
+      assign gen[i] = A[i] & B[i];
+      fulladder fa ((.a(A[i])),(.b(B[i])),(.cin(car[i])),(.sum([i])),(.cout()))
+    end
+  endgenerate
+    
+  assign result = {car[N], sum};
+ 
 endmodule: carry_lookahead_adder
 
