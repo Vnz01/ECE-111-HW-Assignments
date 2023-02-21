e44d8db9ebc90ca6a909b65d7f6ebf2ee0bc6c3e
Vnz01 <111817997+Vnz01@users.noreply.github.com>
2023-02-21 01:07:43 -0800

    Update clock_divide_by_3.sv

--- ECE-111-HW-Assignments/Tech Assignment 6/clock_divide_by_3/clock_divide_by_3.sv
+++ ECE-111-HW-Assignments/Tech Assignment 6/clock_divide_by_3/clock_divide_by_3.sv
@@ -2,37 +2,35 @@ module clock_divide_by_3(
   input  logic clkin, reset,
   output logic clkout);
 
- logic cnt;
- logic ctr, rctr;
+ logic [1:0] ctr, rctr;
 
- //posedge clk that uses mod 3 counter and send the data to the or gate
- always_ff@(posedge clkin, posedge reset) begin
-  if (reset == 1) begin
-     cnt <= 0;
+ always_ff@(posedge clkin) begin
+  if (reset) begin
+  	ctr <= 0;
   end
+  else if(ctr == 2) begin
+  	ctr <= 0;
+  end  
   else begin
-   if(cnt == 2) begin
-        ctr <= 1;
-	cnt <= 0;
-   end  
-   else begin
-     cnt = cnt + 1;
-   end
+  	ctr <= ctr + 1;
   end
- end
+  end
 
- 
- // negedge clock that creates d flip flop in else begin statment 
-always_ff@(negedge clkin, posedge reset) begin
-     if (reset == 1) begin
-     cnt <= 0;
-   end
-   else begin
-     if (clkin) begin
-	rctr <= ctr;
-end
-   end
- end
+ always_ff@(negedge clkin) begin
+  if (reset) begin
+  	rctr <= 0;
+  end
+  else if(rctr == 2) begin
+  	rctr <= 0;
+  end  
+  else begin
+  	rctr <= rctr + 1;
+  end
+  end
+
+  assign clkout = ((ctr == 2)|(rctr == 2));
+
+endmodule: clock_divide_by_3
  
 // clkout or gate
   assign clkout = (ctr || rctr);
