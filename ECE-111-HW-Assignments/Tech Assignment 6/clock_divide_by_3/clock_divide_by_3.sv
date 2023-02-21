eef53d4614d11eecd680800c1811139664e0a2fd
Vnz01 <111817997+Vnz01@users.noreply.github.com>
2023-02-21 00:33:48 -0800

    Update clock_divide_by_3.sv

--- ECE-111-HW-Assignments/Tech Assignment 6/clock_divide_by_3/clock_divide_by_3.sv
+++ ECE-111-HW-Assignments/Tech Assignment 6/clock_divide_by_3/clock_divide_by_3.sv
@@ -1,19 +1,39 @@
-//clock divide by 3 RTL code
-module clock_divide_by_3 ( 
- input  logic clkin, reset,
- output logic clkout);
-  
-logic ctr = 0;
+module clock_divide_by_3( 
+  input  logic clkin, reset,
+  output logic clkout);
 
- // Add clock divide by 3 code
- always @(posedge clkin)
-  if (reset == 1) 
-   clkout <= 0;
- else begin
-  if ((ctr % 2) == 0) begin
-   clkout <= ~clkout;
+ logic cnt;
+ logic ctr, rctr;
+
+ //posedge clk that uses mod 3 counter and send the data to the or gate
+ always_ff@(posedge clkin, posedge reset) begin
+  if (reset == 1) begin
+     cnt <= 0;
   end
-  ctr++;
+  else begin
+   if(cnt == 2) begin
+        ctr <= 1;
+	cnt <= 0;
+   end  
+   else begin
+     cnt = cnt + 1;
+   end
+  end
  end
 
+ 
+ // negedge clock that creates d flip flop in else begin statment 
+always_ff@(negedge clkin, posedge reset) begin
+     if (reset == 1) begin
+     cnt <= 0;
+   end
+   else begin
+     if (clkin) begin
+	rctr <= ctr;
+end
+   end
+ end
+ 
+// clkout or gate
+  assign clkout = (ctr || rctr);
 endmodule: clock_divide_by_3
