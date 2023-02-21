4ca342f6bb5506d227d7767e1cb3a86429dc0263
Vnz01 <111817997+Vnz01@users.noreply.github.com>
2023-02-20 21:43:38 -0800

    Update gray_code_to_binary_convertor.sv

--- ECE-111-HW-Assignments/Tech Assignment 6/gray_code_to_binary_convertor/gray_code_to_binary_convertor.sv
+++ ECE-111-HW-Assignments/Tech Assignment 6/gray_code_to_binary_convertor/gray_code_to_binary_convertor.sv
@@ -3,10 +3,12 @@ module gray_code_to_binary_convertor #(parameter N = 4)(
   input logic[N-1:0] gray_value,
   output logic[N-1:0] binary_value);
  
-  always @ (posedge clock and negedge rstn)
+  always @(posedge clk or negedge rstn)
     begin
       binary_value[N-1] = gray_value[N-1];
-      binary_value[N-2:0] = gray_value[N-2:0] ^ binary_value[N-1:1];
+	for (int i = N-2; i>=0; i--) begin
+        binary_value[i] = gray_value[i] ^ binary_value[i+1];
+	end
     end
 
 endmodule: gray_code_to_binary_convertor
