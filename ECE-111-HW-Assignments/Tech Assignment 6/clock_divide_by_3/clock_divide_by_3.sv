88a5520fc9e36579b093d1695a6d51dfbe3b3477
Vnz01 <111817997+Vnz01@users.noreply.github.com>
2023-02-21 01:40:14 -0800

    Update clock_divide_by_3.sv

--- ECE-111-HW-Assignments/Tech Assignment 6/clock_divide_by_3/clock_divide_by_3.sv
+++ ECE-111-HW-Assignments/Tech Assignment 6/clock_divide_by_3/clock_divide_by_3.sv
@@ -2,22 +2,26 @@ module clock_divide_by_3(
   input  logic clkin, reset,
   output logic clkout);
 
+ logic cnt = 1;
  logic [1:0] ctr, rctr;
 
-  // ctr is posedge counter that resets when it is equal to 2 on posedge of clkin
  always_ff@(posedge clkin) begin
   if (reset) begin
   	ctr <= 0;
   end
   else if(ctr == 2) begin
   	ctr <= 0;
-  end  
+  end
   else begin
+	if(!cnt) begin
   	ctr <= ctr + 1;
+  	end else begin
+	ctr <= 2;
+	cnt <= 0;
+	end
   end
   end
 
-  // rctr is a negedge counter that resets when it is equal to 2 on negedge of clkin
  always_ff@(negedge clkin) begin
   if (reset) begin
   	rctr <= 0;
@@ -29,8 +33,7 @@ module clock_divide_by_3(
   	rctr <= rctr + 1;
   end
   end
- // is returns the output clock to 1 when either ctr or rctr is 2
+
   assign clkout = ((ctr == 2)|(rctr == 2));
 
 endmodule: clock_divide_by_3
- 
