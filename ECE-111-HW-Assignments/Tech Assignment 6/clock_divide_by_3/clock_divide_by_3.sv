341d307f4c67a01561fbad8d7d868702ff1b8c10
Vnz01 <111817997+Vnz01@users.noreply.github.com>
2023-02-20 22:50:37 -0800

    Update clock_divide_by_3.sv

--- ECE-111-HW-Assignments/Tech Assignment 6/clock_divide_by_3/clock_divide_by_3.sv
+++ ECE-111-HW-Assignments/Tech Assignment 6/clock_divide_by_3/clock_divide_by_3.sv
@@ -2,6 +2,8 @@
 module clock_divide_by_3 ( 
  input  logic clkin, reset,
  output logic clkout);
+  
+logic ctr = 0;
 
  // Add clock divide by 3 code
  always @(posedge clkin)
@@ -9,9 +11,8 @@ module clock_divide_by_3 (
    clkout <= 0;
  else begin
   ctr++;
-  if (ctr == 2) begin
-  ctr <= 0;
-   ctr <= ~ctr;
+  if ((ctr % 1) == 0) begin
+   clkout <= ~clkout;
   end
  end
 
