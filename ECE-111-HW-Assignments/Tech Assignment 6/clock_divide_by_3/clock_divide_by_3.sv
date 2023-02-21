d740065af8b59922d0ec177757ee532de4e89d11
Vnz01 <111817997+Vnz01@users.noreply.github.com>
2023-02-20 21:53:21 -0800

    Update clock_divide_by_3.sv

--- ECE-111-HW-Assignments/Tech Assignment 6/clock_divide_by_3/clock_divide_by_3.sv
+++ ECE-111-HW-Assignments/Tech Assignment 6/clock_divide_by_3/clock_divide_by_3.sv
@@ -16,29 +16,3 @@ module clock_divide_by_3 (
  end
 
 endmodule: clock_divide_by_3
-
-module clock_divider_by_3(
-  input logic clkin,    // input clock signal
-  input logic reset,   // synchronous reset signal
-  output logic clkout  // output clock signal
-);
-
-  // counter to divide clock by 3
-  logic [1:0] count;
-
-  always_ff @(posedge clkin, negedge reset) begin
-    if (reset == 0) begin
-      // reset counter to 0
-      count <= 2'b00;
-      clkout <= 0;
-    end else begin
-      // increment counter on each rising edge of input clock
-      count <= count + 1;
-      if (count == 2'b10) begin
-        // output clock signal when counter reaches 2 (i.e. divide by 3)
-        clkout <= ~clkout;
-      end
-    end
-  end
-
-endmodule
