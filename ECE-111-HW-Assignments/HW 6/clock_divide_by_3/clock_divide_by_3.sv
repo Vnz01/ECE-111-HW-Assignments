16e56537dbfd14fc219a002e36e1d665cb6e3d32
Venz Burgos <venz.burgos@yahoo.com>
2023-02-26 17:26:39 -0800

    HW 7

--- ECE-111-HW-Assignments/HW 6/clock_divide_by_3/clock_divide_by_3.sv
+++ ECE-111-HW-Assignments/HW 6/clock_divide_by_3/clock_divide_by_3.sv
@@ -1,39 +1,8 @@
-module clock_divide_by_3( 
-  input  logic clkin, reset,
-  output logic clkout);
+//clock divide by 3 RTL code
+module clock_divide_by_3 ( 
+ input  logic clkin, reset,
+ output logic clkout);
 
- logic cnt = 1;
- logic [1:0] ctr, rctr;
-
- always_ff@(posedge clkin) begin
-  if (reset) begin
-  	ctr <= 0;
-  end
-  else if(ctr == 2) begin
-  	ctr <= 0;
-  end
-  else begin
-	if(!cnt) begin
-  	ctr <= ctr + 1;
-  	end else begin
-	ctr <= 2;
-	cnt <= 0;
-	end
-  end
-  end
-
- always_ff@(negedge clkin) begin
-  if (reset) begin
-  	rctr <= 0;
-  end
-  else if(rctr == 2) begin
-  	rctr <= 0;
-  end  
-  else begin
-  	rctr <= rctr + 1;
-  end
-  end
-
-  assign clkout = ((ctr == 2)|(rctr == 2));
+ // Add clock divide by 3 code
 
 endmodule: clock_divide_by_3
