a9e17270b1a3ed27cee2ff153db59e3a95a8f9dc
Venz Burgos <venz.burgos@yahoo.com>
2023-03-06 15:37:47 -0800

    Update bmc101.sv

--- ECE-111-HW-Assignments/Final Project/vitassign/bmc101.sv
+++ ECE-111-HW-Assignments/Final Project/vitassign/bmc101.sv
@@ -1,6 +1,19 @@
-module bmc101			  (
+module bmc101			  
+(
    input    [1:0] rx_pair,
    output   [1:0] path_0_bmc,
    output   [1:0] path_1_bmc);
 
+logic tmp01 = rx_pair[0];
+logic tmp00 = rx_pair[1];
+logic tmp10;
+logic tmp11;
+
+tmp10 = !tmp00;
+tmp11 = !tmp01;
+
+assign path_0_bmc[1] = tmp00 & tmp01;
+assign path_0_bmc[0] = tmp00 ^ tmp01;
+assign path_1_bmc[1] = tmp10 & tmp11;
+assign path_1_bmc[0] = tmp10 & tmp11;
 endmodule
