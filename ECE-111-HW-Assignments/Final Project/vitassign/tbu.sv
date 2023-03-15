c9b268e8efe814768b6628c53086f043e6075e93
Venz Burgos <venz.burgos@yahoo.com>
2023-03-14 21:14:20 -0700

    Update tbu.sv

--- ECE-111-HW-Assignments/Final Project/vitassign/tbu.sv
+++ ECE-111-HW-Assignments/Final Project/vitassign/tbu.sv
@@ -39,4 +39,131 @@ from selection, d_in_1[pstate], d_in_0[pstate]
 See assignment text for details
 */
 
+	assign wr_en_reg = selection;
+	assign d_o_reg = (selection)? d_in_1[pstate]:0;
+	
+	always_comb begin
+		case(pstate)
+			3'b000: begin
+				if (!selection) begin
+					case(d_in_0[pstate])
+						1'b0: nstate = 3'b000;
+						1'b1: nstate = 3'b001;
+					endcase
+				end
+				else begin
+					case(d_in_1[pstate])
+						1'b0: nstate = 3'b000;
+						1'b1: nstate = 3'b001;
+					endcase
+				end
+			end
+			
+			3'b001: begin
+				if (!selection) begin
+					case(d_in_0[pstate])
+						1'b0: nstate = 3'b011;
+						1'b1: nstate = 3'b010;
+					endcase
+				end
+				else begin
+					case(d_in_1[pstate])
+						1'b0: nstate = 3'b011;
+						1'b1: nstate = 3'b010;
+					endcase
+				end
+			end			
+			
+			3'b010: begin
+				if (!selection) begin
+					case(d_in_0[pstate])
+						1'b0: nstate = 3'b100;
+						1'b1: nstate = 3'b101;
+					endcase
+				end
+				else begin
+					case(d_in_1[pstate])
+						1'b0: nstate = 3'b100;
+						1'b1: nstate = 3'b101;
+					endcase
+				end
+			end
+
+			3'b011: begin
+				if (!selection) begin
+					case(d_in_0[pstate])
+						1'b0: nstate = 3'b111;
+						1'b1: nstate = 3'b110;
+					endcase
+				end
+				else begin
+					case(d_in_1[pstate])
+						1'b0: nstate = 3'b111;
+						1'b1: nstate = 3'b110;
+					endcase
+				end
+			end
+
+			3'b100: begin
+				if (!selection) begin
+					case(d_in_0[pstate])
+						1'b0: nstate = 3'b001;
+						1'b1: nstate = 3'b000;
+					endcase
+				end
+				else begin
+					case(d_in_1[pstate])
+						1'b0: nstate = 3'b001;
+						1'b1: nstate = 3'b000;
+					endcase
+				end
+			end
+
+			3'b101: begin
+				if (!selection) begin
+					case(d_in_0[pstate])
+						1'b0: nstate = 3'b010;
+						1'b1: nstate = 3'b011;
+					endcase
+				end
+				else begin
+					case(d_in_1[pstate])
+						1'b0: nstate = 3'b010;
+						1'b1: nstate = 3'b011;
+					endcase
+				end
+			end
+
+			3'b110: begin
+				if (!selection) begin
+					case(d_in_0[pstate])
+						1'b0: nstate = 3'b101;
+						1'b1: nstate = 3'b100;
+					endcase
+				end
+				else begin
+					case(d_in_1[pstate])
+						1'b0: nstate = 3'b101;
+						1'b1: nstate = 3'b100;
+					endcase
+				end
+			end
+
+			3'b111: begin
+				if (!selection) begin
+					case(d_in_0[pstate])
+						1'b0: nstate = 3'b110;
+						1'b1: nstate = 3'b111;
+					endcase
+				end
+				else begin
+					case(d_in_1[pstate])
+						1'b0: nstate = 3'b110;
+						1'b1: nstate = 3'b111;
+					endcase
+				end
+			end			
+					
+		endcase
+	end
 endmodule
