e28be8c321025e383ff47a2a29eede1458c97ecd
Venz Burgos <venz.burgos@yahoo.com>
2023-03-14 23:17:49 -0700

    Update bmc000.sv

--- ECE-111-HW-Assignments/Final Project/vitassign/bmc000.sv
+++ ECE-111-HW-Assignments/Final Project/vitassign/bmc000.sv
@@ -4,15 +4,19 @@ module bmc000				  // branch metric computation
    output   [1:0] path_0_bmc,
    output   [1:0] path_1_bmc);
 
-logic tmp00 = rx_pair[0];
-logic tmp01 = rx_pair[1];
+logic tmp00;
+logic tmp01;
 logic tmp10;
 logic tmp11;
 
+assign tmp00 = rx_pair[0];
+assign tmp01 = rx_pair[1];
 assign tmp10 = !tmp00;
 assign tmp11 = !tmp01;
+
 assign path_0_bmc[1] = tmp00 & tmp01;
 assign path_0_bmc[0] = tmp00 ^ tmp01;
 assign path_1_bmc[1] = tmp10 & tmp11;
 assign path_1_bmc[0] = tmp10 & tmp11;
 endmodule
+
