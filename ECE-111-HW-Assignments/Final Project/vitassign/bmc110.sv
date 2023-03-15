5cde3440ec273e44132c0c9a79462e7d85d20795
Venz Burgos <venz.burgos@yahoo.com>
2023-03-14 23:19:18 -0700

    Update bmc110.sv

--- ECE-111-HW-Assignments/Final Project/vitassign/bmc110.sv
+++ ECE-111-HW-Assignments/Final Project/vitassign/bmc110.sv
@@ -4,11 +4,13 @@ module bmc110
    output   [1:0] path_0_bmc,
    output   [1:0] path_1_bmc);
 
-logic tmp00 = rx_pair[0];
-logic tmp01 = rx_pair[1];
+logic tmp00;
+logic tmp01;
 logic tmp10;
 logic tmp11;
 
+assign tmp00 = rx_pair[0];
+assign tmp01 = rx_pair[1];
 assign tmp10 = !tmp00;
 assign tmp11 = !tmp01;
 
@@ -17,4 +19,3 @@ assign path_0_bmc[0] = tmp00 ^ tmp01;
 assign path_1_bmc[1] = tmp10 & tmp11;
 assign path_1_bmc[0] = tmp10 & tmp11;
 endmodule
-
